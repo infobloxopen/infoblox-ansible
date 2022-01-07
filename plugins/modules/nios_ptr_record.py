30492ad77d18db50a07810f2648aeea583f9cf73
Anagha K H <70952486+anagha-infoblox@users.noreply.github.com>
2022-01-07 16:39:39 +0530

    Fix to create PTR record in different network views (#103)
    * Fix to create PTR record in different network views
    * Added environment variable in ansible-test.yaml

--- plugins/modules/nios_ptr_record.py
+++ plugins/modules/nios_ptr_record.py
@@ -120,7 +120,7 @@ def main():
     # Module entry point
     ib_spec = dict(
         name=dict(required=False),
-        view=dict(default='default', aliases=['dns_view']),
+        view=dict(default='default', aliases=['dns_view'], ib_req=True),
         ipv4addr=dict(aliases=['ipv4'], ib_req=True),
         ipv6addr=dict(aliases=['ipv6'], ib_req=True),
         ptrdname=dict(required=True, ib_req=True),
