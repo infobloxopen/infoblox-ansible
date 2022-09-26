670a9dc3752103172bc98795477a6fe7e8c4dc4e
Matt <dennett.matthew@gmail.com>
2022-09-26 15:26:13 +1000

    Fixed bug in error check

--- plugins/module_utils/api.py
+++ plugins/module_utils/api.py
@@ -184,10 +184,8 @@ def member_normalize(member_spec):
 
 def convert_range_member_to_struct(member_spec):
     # Error checking that only one member type was defined
-
-    # BUG - 
     opts = list(set(member_spec.keys()).intersection(['member', 'failover_association', 'ms_server']))
-    if len(opts) < 1:
+    if len(opts) > 1:
         raise AttributeError("'%s' can not be defined when '%s' is defined!" % (opts[0], opts[1]))
 
     # A member node was passed in. Ehsure the correct type and struct
