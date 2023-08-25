4ecb56288735215d6ce37bc373aacee55e7a0139
Joachim Buyse <jbuyse@isabelgroup.eu>
2023-08-25 14:34:25 +0200

    fix: better error handling

--- plugins/modules/nios_dtc_lbdn.py
+++ plugins/modules/nios_dtc_lbdn.py
@@ -205,7 +205,7 @@ def main():
         topology = module.params['topology']
         if topology:
             topo_obj = wapi.get_object('dtc:topology', {'name': topology})
-            if topo_obj is not None:
+            if topo_obj:
                 return topo_obj[0]['_ref']
             else:
                 module.fail_json(
