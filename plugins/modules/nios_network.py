73f82047f5e5013aa0ad216b2d977d5705c16f1c
Jaykumar Chhatbar <45447272+JchhatbarInfoblox@users.noreply.github.com>
2024-07-25 11:25:45 +0530

    [NPA-83] Fixed DHCP options for IPv4/v6 network container. (#243)
    * [NPA-83] Fixed dhcp options for IPv4/v6 network container.

--- plugins/modules/nios_network.py
+++ plugins/modules/nios_network.py
@@ -315,7 +315,6 @@ def check_ip_addr_type(obj_filter, ib_spec):
     if 'container' in obj_filter and obj_filter['container']:
         check_ip = ip.split('/')
         del ib_spec['container']  # removing the container key from post arguments
-        del ib_spec['options']  # removing option argument as for network container it's not supported
         if validate_ip_address(check_ip[0]):
             return NIOS_IPV4_NETWORK_CONTAINER, ib_spec
         elif validate_ip_v6_address(check_ip[0]):
