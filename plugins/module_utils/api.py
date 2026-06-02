e8e040769762c8ff94ba9cacbd248bd1e307e6db
Ryan Badaï <43403063+ryanb74@users.noreply.github.com>
2026-06-02 10:30:06 +0200

    fix(api.py): fix import statements deprecation warning (#316)
    * fix(api.py): fix import statements deprecation warning
    
    fixes the warning related to `to_native` and `to_text` functions import
    
    * chore: add changelog fragment

--- plugins/module_utils/api.py
+++ plugins/module_utils/api.py
@@ -33,8 +33,8 @@ import json
 import os
 import copy
 from functools import partial
-from ansible.module_utils._text import to_native
-from ansible.module_utils._text import to_text
+from ansible.module_utils.common.text.converters import to_native
+from ansible.module_utils.common.text.converters import to_text
 from ansible.module_utils.basic import env_fallback
 from ansible.module_utils.common.validation import check_type_dict
 from ast import literal_eval as safe_eval
